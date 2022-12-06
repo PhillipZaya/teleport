ff04c3b0cf25f769861b525b3e67f4c9557a83a0
Phillip Zaya <Phillip.zaya@outlook.com>
2022-12-06 12:36:42 +1100

    first commit

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/phillipzaya/Desktop/kaleidoscopic medical termite-gatsby/.cache/dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/phillipzaya/Desktop/kaleidoscopic medical termite-gatsby/src/pages/index.js"))
+}
+
